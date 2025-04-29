ff62dd192c23eba43c0f236a3c5c6ca3a72166ac
Harry101969 <harshagarwal19696@gmail.com>
2025-04-29 21:26:54 +0530

    Updated

---
+++ learning/inputs.py
@@ -0,0 +1,9 @@
+brith_year = int(input("Whats ur age: "))
+print(type(brith_year))
+age=2025-brith_year
+print(type(age))
+print(age)
+course = 'My name is "Harsh Agarwal!"'
+print(course)
+print(course[9])
+print(course[3:6])  # this will print the index from 3 to 6 but will exclude 6 and will print only uptill 5th index only
